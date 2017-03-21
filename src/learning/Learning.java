5ee18447595e8cc5fc569c8d147623c90da759a8
RaminKam <raminkamalov@gmail.com>
2017-03-22 00:28:10 +0400

    repair some code, now we have minimal functionality

--- src/learning/Learning.java
+++ src/learning/Learning.java
@@ -3,9 +3,9 @@ import java.util.*;
 import java.io.*;
 public class Learning
 {
-	static final int time1=5000;
+	static final int time1=1000;
 	static final int time2=2000;
-	String fdir="//storage//emulated//0//SimpleNotepad2//dict-[01-02-2016].txt";
+	String fdir="//storage//emulated//0//SimpleNotepad2//dict-[01-02-2016].txt";  //1490126478692.txt";  //dict-[01-02-2016].txt" ;
 	ArrayList<String> arr = new ArrayList<>();
 	public Learning(){
 
@@ -19,21 +19,20 @@ public class Learning
 			String strfor=new String();
 			while(ch!=-1){
 				//System.out.print((char)ch);
-
-				if((char)ch!='\n')
-					strfor=strfor+(char)ch;
-				ch=fstr.read();
 				if((char)ch=='\n'){
-
 					arr.add(strfor);
+					System.out.println(":"+strfor);
 					strfor=new String();
+				}else {
+					strfor=strfor+(char)ch;
 				}
+				ch=fstr.read();
 			}
-
-
 			arr.add(strfor);
+			System.out.println(":"+strfor+":");
+			System.out.println("size="+arr.size());
 			for(int g=0;g<arr.size();g++){
-				//System.out.print(arr.get(g));
+				System.out.print(arr.get(g));
 			}
 			//System.out.println();
 			for(int y=0;y<arr.get(1).length();y++){
@@ -58,23 +57,31 @@ public class Learning
 		for(int g=0;g<arr.size();g++){
 			String t=arr.get(g);
 			spls =t.split("-");
+			//if string have not word pairs:
+			//плохой стиль - нкжны свои классы
+			//исключений
+			
+			if (spls.length == 2)
+			{
 
-			for(int v=0;v<spls.length;v++){
-				//System.out.print(v+"-"+"{"+spls[v]+"}");
+				for (int v=0;v < spls.length;v++)
+				{
+					System.out.print(v + "-" + "{" + spls[v] + "}");
+				}
+				eng.add(spls[0]);
+				rus.add(spls[1]);
 			}
-			eng.add(spls[0]);
-			rus.add(spls[1]);
 
 		}
-		//System.out.println(Arrays.toString(eng.toArray()));
+		System.out.println(Arrays.toString(eng.toArray()));
 
-		Random rand=new Random();
+		
 		int mode=(int)(Math.random()*10)%2;
 		System.out.println("mode"+mode);
 		ArrayList<String> aa=(ArrayList<String>)eng.clone();
 		ArrayList<String> bb=(ArrayList<String>)rus.clone();
 		while(aa.size()>0){
-			int ind=rand.nextInt(aa.size());
+			int ind=(int)(Math.random()*100)%aa.size();
 			System.out.println("\n\n\n\n\n");
 			if(mode==1)
 				System.out.println("\n"+aa.get(ind));
--- src/main/Main.java
+++ src/main/Main.java
@@ -6,7 +6,7 @@ import learning.Learning;
 public class Main
 {
 	public static void main(String[] args){
-		System.out.println("hi pidor!");
+		System.out.println("");
 		ReadConfigFile.getConfig();
 		Learning ln=new Learning();
 		ln.readFileThere();
